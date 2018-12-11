3fad667747af7ea5d8df900f479786c3d1226757
[name] <prashant.patchi@gmail.com>
2018-12-11 11:42:24 +0100

    Initial commit

---
+++ app/src/main/java/win/prashant/fragmenttesting/Fragmentlogin.java
@@ -0,0 +1,24 @@
+package win.prashant.fragmenttesting;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragmentlogin extends Fragment {
+
+    @Nullable
+    @Override
+
+    // layout inflater to make sure with layout need to be connected
+    //OnCreateview is for fragment call for activity is for onCreate
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        //to access the fragment from Resource inside layout then name of fragment
+        View view = inflater.inflate(R.layout.fragmentlogin,container,false);
+        return view;
+    }
+}
